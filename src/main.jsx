dcda28134c25d01762e78fcdadda73157b86036c
purnachandra057 <lightvelocity888@gmail.com>
2023-01-23 07:16:11 +0545

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <p>Welcome to Mutex</p>
+  </React.StrictMode>,
+)
