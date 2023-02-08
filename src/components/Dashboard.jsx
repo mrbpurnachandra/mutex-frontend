d65b2808cfa0d75145da98212b07e9778502a2b7
purnachandra057 <lightvelocity888@gmail.com>
2023-02-08 20:43:58 +0545

    WIP"

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -22,8 +22,37 @@ function SideBar() {
             <div>
                 <Brand />
                 <div className='p-4'>
-                    {isStudent && <StudentChannel />}
-                    {isTeacher && <TeacherChannel />}
+                    {isStudent && (
+                        <NavLink
+                            to={`/message/home`}
+                            className={({ isActive }) =>
+                                isActive
+                                    ? 'mt-2 flex items-center space-x-4 px-4 py-2 text-blue-100 bg-blue-600 rounded-md '
+                                    : 'mt-2 flex items-center space-x-4 px-4 py-2 text-gray-800  rounded-md hover:bg-blue-200'
+                            }
+                        >
+                            <span>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    strokeWidth={1.5}
+                                    stroke='currentColor'
+                                    className='w-6 h-6'
+                                >
+                                    <path
+                                        strokeLinecap='round'
+                                        strokeLinejoin='round'
+                                        d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
+                                    />
+                                </svg>
+                            </span>
+                            <p className='flex flex-col leading-tight'>
+                                <span>Home</span>
+                            </p>
+                        </NavLink>
+                    )}
+                    <CommonChannel />
                 </div>
             </div>
             <div className='border-t border-gray-300 bg-gray-200 p-4'>
@@ -109,7 +138,7 @@ function SideBar() {
     )
 }
 
-function StudentChannel() {
+function CommonChannel({ isStudent }) {
     const lecturerQuery = useQuery({
         queryFn: () => {
             return httpClient.get('/lecture')
@@ -121,7 +150,34 @@ function StudentChannel() {
     if (lecturerQuery.isLoading) return <div>Loading...</div>
     if (lecturerQuery.error) return <ErrorElement error={lecturerQuery.error} />
     return (
-        <div>
+        <>
+            <NavLink
+                to={`/message/announcements`}
+                className={({ isActive }) =>
+                    isActive
+                        ? 'mt-2 flex items-center space-x-4 px-4 py-2 text-blue-100 bg-blue-600 rounded-md '
+                        : 'mt-2 flex items-center space-x-4 px-4 py-2 text-gray-800  rounded-md hover:bg-blue-200'
+                }
+            >
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='w-6 h-6'
+                >
+                    <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46'
+                    />
+                </svg>
+
+                <p className='flex flex-col leading-tight'>
+                    <span>Announcements</span>
+                </p>
+            </NavLink>
             {lecturerQuery.data.map((lecture) => {
                 const classId = lecture.class.id
                 const teacherId = lecture.teacher.id
@@ -153,21 +209,22 @@ function StudentChannel() {
                             </svg>
                         </span>
                         <p className='flex flex-col leading-tight'>
-                            <span>{lecture.teacher.user.name}</span>
-                            <span className='text-sm'>
-                                @{lecture.teacher.user.username}
+                            <span>
+                                {isStudent
+                                    ? lecture.teacher.user.name
+                                    : lecture.class.name}
                             </span>
+                            {isStudent && (
+                                <span className='text-sm'>
+                                    @{lecture.teacher.user.username}
+                                </span>
+                            )}
                         </p>
                     </NavLink>
                 )
             })}
-        </div>
+        </>
     )
-}
-
-// TODO - Wrok here
-function TeacherChannel() {
-    return <p>Teacher's navigation</p>
 }
 
 function ContentArea() {
